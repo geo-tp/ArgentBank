dff2728548d80ded4e5f28426fe91fd9fd367b36
geo <tp53.geo@gmail.com>
2022-11-03 22:10:13 +0100

    add comments for store constantes

--- src/store/constants/user.js
+++ src/store/constants/user.js
@@ -1,7 +1,9 @@
+// Get Profile
 export const GET_USER = "GET_USER";
 export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
 export const GET_USER_ERROR = "GET_USER_ERROR";
 
+// Update Profile
 export const UPDATE_USER = "UPDATE_USER";
 export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
 export const UPDATE_USER_ERROR = "UPDATE_USER_ERROR";
